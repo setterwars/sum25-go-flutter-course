0967f92d24b1c4fee66c6536c81dc03e5c279199
setterwars <s.zaynulin@innopolis.university>
2025-06-23 07:17:58 +0300

    complited lab 1

--- labs/lab01/backend/calculator/calculator.go
+++ labs/lab01/backend/calculator/calculator.go
@@ -29,7 +29,8 @@ func Divide(a, b float64) (float64, error) {
 	if b == 0 {
 		return 0, ErrDivisionByZero
 	}
-	return a / b, nil
+	result := a / b
+	return result, nil
 }
 
 // StringToFloat converts a string to float64
